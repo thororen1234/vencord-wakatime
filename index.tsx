8f8a543815b1a0b86387b6f8737a6f15b5342611
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-07-18 23:54:59 -0400

    Fix Up Wakatime

--- index.tsx
+++ index.tsx
@@ -4,30 +4,29 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import definePlugin, { OptionType } from '@utils/types';
+import definePlugin, { OptionType } from "@utils/types";
 import { showNotification } from "@api/Notifications";
-import { definePluginSettings } from '@api/Settings';
+import { definePluginSettings } from "@api/Settings";
 
 const settings = definePluginSettings({
     apiKey: {
         type: OptionType.STRING,
-        description: 'API Key for wakatime',
-        default: '',
-        isValid: (e: string) => {
-            if (!e) return "Invalid Key: Please change the default API Key";
-            if (!e.startsWith("waka_")) return "Invalid Key: Key must start with 'waka_'";
+        description: "API Key for Wakatime",
+        default: "",
+        isValid: (value: string) => {
+            if (!value?.startsWith("waka_")) return "Invalid Key: Please obtain your API Key from Wakatime";
             return true;
         },
     },
     debug: {
         type: OptionType.BOOLEAN,
-        description: 'Enable debug mode',
+        description: "Enable debug mode",
         default: false,
     },
     machineName: {
         type: OptionType.STRING,
-        description: 'Machine name',
-        default: 'Vencord User',
+        description: "Machine name",
+        default: "Vencord User",
     },
     projectName: {
         type: OptionType.STRING,
@@ -36,62 +35,52 @@ const settings = definePluginSettings({
     },
 });
 
-async function sendHeartbeat(time) {
-    const key = settings.store.apiKey;
-    if (!key) {
-        showNotification({
-            title: "WakaTime",
-            body: "No api key for wakatime is setup.",
-            color: "var(--red-360)",
-        });
+async function sendHeartbeat() {
+    const time = Date.now();
+    const { debug, apiKey, machineName, projectName } = settings.store
 
-        return;
-    }
-    if (settings.store.debug) {
-        console.log('Sending heartbeat to WakaTime API.');
-    }
+    if (debug) console.log("Sending heartbeat to WakaTime API.");
 
-    const url = 'https://api.wakatime.com/api/v1/users/current/heartbeats';
+    const url = "https://api.wakatime.com/api/v1/users/current/heartbeats";
     const body = JSON.stringify({
         time: time / 1000,
-        entity: 'Discord',
-        type: 'app',
-        project: settings.store.projectName ?? "Discord",
-        plugin: 'vencord/version discord-wakatime/v0.0.1',
+        entity: "Discord",
+        type: "app",
+        project: projectName ?? "Discord",
+        plugin: "vencord/version discord-wakatime/v0.0.1",
     });
+
     const headers = {
-        Authorization: `Basic ${key}`,
-        'Content-Type': 'application/json',
-        'Content-Length': new TextEncoder().encode(body).length.toString(),
+        Authorization: `Basic ${apiKey}`,
+        "Content-Type": "application/json",
+        "Content-Length": new TextEncoder().encode(body).length.toString(),
+        ...(machineName ? { "X-Machine-Name": machineName } : {})
     };
-    const machine = settings.store.machineName;
-    if (machine) headers['X-Machine-Name'] = machine;
-    const response = await fetch(url, {
-        method: 'POST',
+
+    const res = await fetch(url, {
+        method: "POST",
         body: body,
         headers: headers,
     });
-    const data = await response.text();
-    if (response.status < 200 || response.status >= 300) console.warn(`WakaTime API Error ${response.status}: ${data}`);
+
+    const data = await res.text();
+
+    if (res.status !== 200) console.warn(`WakaTime API Error ${res.status}: ${data}`);
 }
 
 export default definePlugin({
-    name: 'Wakatime',
-    description: 'Fully automatic code stats via Wakatime',
+    name: "Wakatime",
+    description: "Fully automatic code stats via Wakatime",
     authors: [
-        {
-            id: 566766267046821888n,
-            name: 'Neon',
-        },
+        { name: "Neon", id: 566766267046821888n },
+        { name: "thororen", id: 848339671629299742n }
     ],
     settings,
+    sendHeartbeat,
     start() {
-        this.updateInterval = setInterval(async () => {
-            const time = Date.now();
-            await sendHeartbeat(time);
-        }, 120000);
+        this.updateInterval = setInterval(() => { this.sendHeartbeat(); }, 120000);
     },
     stop() {
         clearInterval(this.updateInterval);
-    },
+    }
 });
